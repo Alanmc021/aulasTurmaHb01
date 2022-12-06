8c1e9ec8bebb87a55f0bf9d7f012353dd4acfcd9
dev-alan <alanmc021@gmail.com>
2022-12-06 20:47:06 -0300

    first commit

---
+++ js/01-tipos primitivos/exercicios.js
@@ -0,0 +1,32 @@
+// 1. Declare uma variável de nome weight
+
+// 2. Que tipo de dado é a variável acima
+
+/* 3. Declare uma variável e atribua valores para cada um dos dados:
+*Name: String
+*age: Number(integer)
+*stars: Number (float)
+*IsSubscribed: Boolean
+*/ 
+/* 4. A variável student abaixo é de que tipo de dados
+4.1 atribua a elas as mesmas propriedades e valores do exercicio 3.
+4.2 Mostre no console a seguinte mensagem <name> de idade <age> pesa <weight> kg.
+*/ 
+ 
+/* 5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja somente o Array vazio
+ */
+ 
+/* 6. Reatribua valor para variável acima, colocando dentro dela o objeto students da questão 4.
+(não copiar e colar o objeto, mas usar o objeto criado e inserir ele no Array)
+*/
+ 
+/* 7. Coloque no console o valor da posição zero do Array acima
+ */
+ 
+
+/* 8. Crie um novo student e coloque na posição 1 do Array students
+ */
+
+
+/* 9. Sem rodar o codigo responda qual a resposta do codigo abaixo e por que, Após sua resposta rode e o codigo e veja se acertou.
+ */
---
+++ js/01-tipos primitivos/index.js
@@ -0,0 +1,40 @@
+//David Flanagan 
+//Tipos primitivos 
+var numero1 = 10;
+var numero2 = 20;
+var numero3 = 30;
+var nome = "Alan martins";
+var validador = false
+
+//tipos primitivos
+// 1 - number , 2 - String , 3 - boolena
+
+//
+
+// alunos = objeto 
+const alunos1 =
+{   
+    id:0001,
+    nome:'Fabio junior',
+    telefone: 222333444,
+    pagamento: true
+};
+
+const alunos2 =
+{   
+    id:0002,
+    nome:'Wladmiro Magleve',
+    telefone: 2222224,
+    pagamento: true
+};
+
+// tipos de estruturas 
+// 1 - Array  2 - Objeto
+
+//lista de alunos = Array 
+const listaDeAlunos = [alunos1, alunos2]
+console.log(listaDeAlunos);
+
+// const colecaoDeNumeros = [numero1 , numero2, numero3]
+
+// console.log(colecaoDeNumeros);
